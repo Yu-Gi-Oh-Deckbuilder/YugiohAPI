8c0eb9bce6428fd8768b3f62048d33ab3d082444
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-04-08 20:41:23 -0700

    deleteuser, updateuser, and creae user services were created with their respectives tests

--- src/test/java/com/revature/main/service/UserServiceTest.java
+++ src/test/java/com/revature/main/service/UserServiceTest.java
@@ -15,6 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
