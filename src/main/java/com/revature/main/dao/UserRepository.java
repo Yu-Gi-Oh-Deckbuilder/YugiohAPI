eb1ba160bc17bc1ef1bb2331d679d753dfa79c47
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-04-08 20:41:23 -0700

    deleteuser, updateuser, and creae user services were created with their respectives tests

--- src/main/java/com/revature/main/dao/UserRepository.java
+++ src/main/java/com/revature/main/dao/UserRepository.java
@@ -1,8 +1,10 @@
 package com.revature.main.dao;
 
 
+import com.revature.main.dto.UserDto;
 import com.revature.main.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
--- src/test/java/com/revature/main/service/UserServiceTest.java
+++ src/test/java/com/revature/main/service/UserServiceTest.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
