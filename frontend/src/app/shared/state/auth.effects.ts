f2c359dc5b15b1991d15c2d9190501a832e6013d
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2022-04-11 16:02:03 -0400

    prelim setup

--- frontend/src/app/shared/state/auth.effects.ts
+++ frontend/src/app/shared/state/auth.effects.ts
@@ -1,13 +1,16 @@
 import { Injectable } from "@angular/core";
-import { Actions } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { map, mergeMap } from "rxjs";
+import { AuthService } from "../auth.service";
+import * as fromActions from "./auth.actions";
 
 @Injectable()
 export class AuthEffects {
 
   constructor(
     private actions$: Actions,
+    private auth: AuthService,
   )
   {}
-
 
 }
