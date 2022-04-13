4cc395f1029cb40da684eca6af518c7278e2ca7b
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2022-04-13 13:28:44 -0400

    Add cors handling to authcontroller

--- src/main/java/com/revature/main/controller/AuthenticationController.java
+++ src/main/java/com/revature/main/controller/AuthenticationController.java
@@ -10,12 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Profile("prod")
 @RestController
+@CrossOrigin
 public class AuthenticationController {
 
     @Autowired
