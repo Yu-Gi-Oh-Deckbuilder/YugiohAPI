2e7f207b87fd6d66a94647cfa7d80d62bcafced8
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-04-15 13:17:12 -0700

    auto inserting data once you start the application, change autogenerate to indentity on models

--- src/main/java/com/revature/main/model/Collection.java
+++ src/main/java/com/revature/main/model/Collection.java
@@ -14,7 +14,7 @@ import java.util.Map;
 public class Collection {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column
